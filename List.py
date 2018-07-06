0f8c73a50d0c0429c68b53d8413d0db50fd76173
André Luiz <andreluiz.cssantos@gmail.com>
2018-07-06 11:34:50 -0300

    Add files via upload

---
+++ List.py
@@ -0,0 +1,107 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+#não precisa definir tamanho nem tipo de dado
+my_list = [1,2,3]
+
+
+# In[3]:
+
+
+my_list
+
+
+# In[4]:
+
+
+my_list2 = [1, 2.2, 'string']
+
+
+# In[5]:
+
+
+my_list2
+
+
+# In[8]:
+
+
+my_list2[1:]
+
+
+# In[9]:
+
+
+my_list2[:-1]
+
+
+# In[10]:
+
+
+my_list + my_list2
+
+
+# In[11]:
+
+
+lst_1 = [1, 2, 3]
+lst_2 = [4, 5, 6]
+lst_3 = [7, 8, 9]
+
+
+# In[12]:
+
+
+matriz = [lst_1, lst_2, lst_3]
+
+
+# In[13]:
+
+
+matriz
+
+
+# In[15]:
+
+
+matriz[1][1]
+
+
+# In[16]:
+
+
+matriz[2][0]
+
+
+# In[17]:
+
+
+matriz[1][2]
+
+
+# In[23]:
+
+
+first_col = [a[0] for a in matriz] #a pode ter qualquer nome
+
+
+# In[22]:
+
+
+first_col
+
+
+# In[25]:
+
+
+lst = ['a','b','c']
+
+
+# In[26]:
+
+
+lst[1:]
+
